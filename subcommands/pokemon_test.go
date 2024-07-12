c0fb3ffcfc44e742abaf0b904248117b152b7f48
Christian <developer@digitalghost.dev>
2024-07-12 09:52:41 -0700

    updating tests

--- subcommands/pokemon_test.go
+++ subcommands/pokemon_test.go
@@ -1,45 +1,54 @@
 package subcommands
 
 import (
+	"github.com/charmbracelet/lipgloss"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
+const red = lipgloss.Color("#F2055C")
+
+var errorColor = lipgloss.NewStyle().Foreground(red)
+
 func TestValidateArgs_ValidInput(t *testing.T) {
 	validInputs := [][]string{
-		{"pikachu"},
-		{"bulbasaur", "--types"},
-		{"cloyster", "--abilities"},
-		{"mewtwo", "--types", "--abilities"},
+		{"poke-cli", "pikachu"},
+		{"poke-cli", "bulbasaur", "--types"},
+		{"poke-cli", "cloyster", "--abilities"},
+		{"poke-cli", "mewtwo", "--types", "--abilities"},
 	}
 
 	for _, input := range validInputs {
-		err := ValidateArgs(input)
+		err := ValidateArgs(input, errorColor)
 		assert.NoError(t, err, "Expected no error for valid input")
 	}
 }
 
 func TestValidateArgs_InvalidFlag(t *testing.T) {
 	invalidInputs := [][]string{
-		{"poke-cli", "pikachu", "wartortle"},
 		{"poke-cli", "bulbasaur", "types"},
+		{"poke-cli", "mewtwo", "--types", "abilities"},
+	}
+	expectedErrors := []string{
+		"Error: Invalid argument 'types'. Only flags are allowed after declaring a Pokémon's name",
+		"Error: Invalid argument 'abilities'. Only flags are allowed after declaring a Pokémon's name",
 	}
 
-	for _, input := range invalidInputs {
-		err := ValidateArgs(input)
+	for i, input := range invalidInputs {
+		err := ValidateArgs(input, errorColor)
 		assert.Error(t, err, "Expected error for invalid flag")
+		assert.NotEmpty(t, expectedErrors[i], err.Error())
 	}
 }
 
 func TestValidateArgs_TooManyArgs(t *testing.T) {
 	invalidInput := [][]string{
-		{"mewtwo", "--types", "all"},
-		{"pikachu", "--types", "all", "normal"},
+		{"poke-cli", "pikachu", "--types", "all", "normal"},
 	}
 	expectedError := "error: too many arguments\n"
 
 	for _, input := range invalidInput {
-		err := ValidateArgs(input)
+		err := ValidateArgs(input, errorColor)
 		assert.Error(t, err, "Expected error for too many arguments")
 		assert.NotEqual(t, expectedError, err.Error())
 	}
