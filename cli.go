db766f6b6aa8a1cb1cc7efcb09522b922a4a2c89
Christian <developer@digitalghost.dev>
2024-08-01 20:24:11 -0700

    updated help menu, added latest flag

--- cli.go
+++ cli.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/digitalghost-dev/poke-cli/flags"
 	"github.com/digitalghost-dev/poke-cli/subcommands"
 )
 
@@ -11,6 +12,9 @@ var styleBold = lipgloss.NewStyle().Bold(true)
 var styleItalic = lipgloss.NewStyle().Italic(true)
 
 func main() {
+
+	latestFlag := flag.Bool("latest", false, "Prints the program's latest Docker Image and Release versions.")
+	shortLatestFlag := flag.Bool("l", false, "Prints the program's latest Docker Image and Release versions.")
 
 	flag.Usage = func() {
 		fmt.Println("Welcome! This tool displays data about a selected Pokémon in the terminal!")
@@ -22,17 +26,22 @@ func main() {
 		fmt.Println("\t", styleItalic.Render("Example:"), "poke-cli bulbasaur", styleItalic.Render("or"), "poke-cli flutter-mane --types")
 
 		fmt.Println(styleBold.Render("\nGLOBAL FLAGS:"))
-		fmt.Println("\t", "-h, --help", "\t", "Shows the help menu")
+		fmt.Println("\t", "-h, --help", "\t\t", "Shows the help menu")
+		fmt.Println("\t", "-l, --latest", "\t\t", "Prints the latest version of the program")
 		fmt.Print("\n")
 
 		fmt.Println(styleBold.Render("POKEMON NAME FLAGS:"))
 		fmt.Println("\t", "Add a flag after declaring a Pokémon's name for more details!")
-		fmt.Println("\t", "--abilities, -a", "\t\t", "Prints out the Pokémon's abilities.")
-		fmt.Println("\t", "--types, -t", "\t\t\t", "Prints out the Pokémon's typing.")
+		fmt.Println("\t", "-a, --abilities", "\t", "Prints out the Pokémon's abilities.")
+		fmt.Println("\t", "-t, --types", "\t\t", "Prints out the Pokémon's typing.")
 		fmt.Print("\n")
 	}
 
 	flag.Parse()
 
-	subcommands.PokemonCommand()
+	if *latestFlag || *shortLatestFlag {
+		flags.LatestFlag()
+	} else {
+		subcommands.PokemonCommand()
+	}
 }
