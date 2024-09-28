ee26db66650d8ac317728fd0cedb11dec67f22fb
Christian <developer@digitalghost.dev>
2024-09-27 20:07:37 -0700

    updating tests

--- cli_test.go
+++ cli_test.go
@@ -81,23 +81,32 @@ func TestCLI(t *testing.T) {
 		},
 		{
 			args: []string{"--help"},
-			expectedOutput: "╭─────────────────────────────────────────────────────────────╮\n" +
-				"│Welcome! This tool displays data related to Pokémon!         │\n" +
-				"│                                                             │\n" +
-				"│ USAGE:                                                      │\n" +
-				"│    poke-cli [flag]                                          │\n" +
-				"│    poke-cli [command] [flag]                                │\n" +
-				"│    poke-cli [command] [subcommand] [flag]                   │\n" +
-				"│                                                             │\n" +
-				"│ FLAGS:                                                      │\n" +
-				"│    -h, --help      Shows the help menu                      │\n" +
-				"│    -l, --latest    Prints the latest version of the program │\n" +
-				"│                                                             │\n" +
-				"│ AVAILABLE COMMANDS:                                         │\n" +
-				"│    pokemon         Get details of a specific Pokémon        │\n" +
-				"│    types           Get details of a specific typing         │\n" +
-				"╰─────────────────────────────────────────────────────────────╯\n",
+			expectedOutput: "╭──────────────────────────────────────────────────────╮\n" +
+				"│Welcome! This tool displays data related to Pokémon!  │\n" +
+				"│                                                      │\n" +
+				"│ USAGE:                                               │\n" +
+				"│    poke-cli [flag]                                   │\n" +
+				"│    poke-cli [command] [flag]                         │\n" +
+				"│    poke-cli [command] [subcommand] [flag]            │\n" +
+				"│                                                      │\n" +
+				"│ FLAGS:                                               │\n" +
+				"│    -h, --help      Shows the help menu               │\n" +
+				"│    -l, --latest    Prints the latest available       │\n" +
+				"│                    version of the program            │\n" +
+				"│                                                      │\n" +
+				"│ AVAILABLE COMMANDS:                                  │\n" +
+				"│    pokemon         Get details of a specific Pokémon │\n" +
+				"│    types           Get details of a specific typing  │\n" +
+				"╰──────────────────────────────────────────────────────╯\n",
 			expectedExit: 0,
+		},
+		{
+			args: []string{"types", "ground"},
+			expectedOutput: "╭───────────────────────────────────────────╮\n" +
+				"│Error! The only currently available options│\n" +
+				"│after [types] command is '-h' or '--help'  │\n" +
+				"╰───────────────────────────────────────────╯\n",
+			expectedExit: 1,
 		},
 	}
 
