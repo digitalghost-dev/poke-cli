a3695ce6638f7b9ddbb5938cc261e5c98bafa3f5
Christian <developer@digitalghost.dev>
2024-09-30 15:01:57 -0700

    initial commit

--- cmd/types_test.go
+++ cmd/types_test.go
@@ -1,1 +1,18 @@
 package cmd
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidateTypesArgs_ValidInput(t *testing.T) {
+	validInputs := [][]string{
+		{"poke-cli", "types"},
+		{"poke-cli", "types", "-h"},
+	}
+
+	for _, input := range validInputs {
+		err := ValidateTypesArgs(input)
+		assert.NoError(t, err, "Expected no error for valid input")
+	}
+}
