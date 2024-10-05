b36d4d3a09ce4dc551c960660b60e715f6a043a2
Christian <developer@digitalghost.dev>
2024-10-05 07:59:02 -0700

    adding TooManyArgs test (#43)

--- cmd/types_test.go
+++ cmd/types_test.go
@@ -16,3 +16,16 @@ func TestValidateTypesArgs_ValidInput(t *testing.T) {
 		assert.NoError(t, err, "Expected no error for valid input")
 	}
 }
+
+func TestValidateTypesArgs_TooManyArgs(t *testing.T) {
+	invalidInputs := [][]string{
+		{"poke-cli", "types", "ground"},
+	}
+	expectedError := "error, too many arguemnts\n"
+
+	for _, input := range invalidInputs {
+		err := ValidateTypesArgs(input)
+		assert.Error(t, err, "Expected error for too many arguments")
+		assert.NotEqual(t, expectedError, err.Error())
+	}
+}
