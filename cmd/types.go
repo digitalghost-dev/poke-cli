355014b5094cca9b14473b28840594fde39f4c0c
Christian <developer@digitalghost.dev>
2024-10-20 11:45:35 -0700

    better code organization (#51)

--- cmd/types.go
+++ cmd/types.go
@@ -13,10 +13,6 @@ import (
 	"strings"
 )
 
-var baseStyle = lipgloss.NewStyle().
-	BorderStyle(lipgloss.NormalBorder()).
-	BorderForeground(lipgloss.Color("#FFCC00"))
-
 type model struct {
 	table          table.Model
 	selectedOption string // Track the selected option
@@ -30,6 +26,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "ctrl+c":
+			// Fully quiting the program while
+			// the table is in selection mode
+			m.selectedOption = "quit"
 			return m, tea.Quit
 		case "enter":
 			selectedRow := m.table.SelectedRow()
@@ -44,49 +43,16 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
+	// When an option is selected, no longer display the table.
 	if m.selectedOption != "" {
 		return ""
 	}
-	return "Select a type! Hit 'Q' or 'CTRL-C' to quit.\n" + baseStyle.Render(m.table.View()) + "\n"
+	// Otherwise, display the table
+	return "Select a type! Hit 'Q' or 'CTRL-C' to quit.\n" + typesTableBorder.Render(m.table.View()) + "\n"
 }
 
-func selectionResult(endpoint, typesName string) error {
-	baseURL := "https://pokeapi.co/api/v2/"
-
-	typeResponse, typeName, _ := connections.TypesApiCall(endpoint, typesName, baseURL)
-
-	capitalizedString := cases.Title(language.English).String(typeName)
-
-	pokemonCount := len(typeResponse.Pokemon)
-
-	fmt.Printf("You selected Type: %s\nNumber of Pokémon with type: %d\n", capitalizedString, pokemonCount)
-	return nil
-}
-
-func TypesCommand() {
-	flag.Usage = func() {
-		helpMessage := helpBorder.Render(
-			styleBold.Render("USAGE:"),
-			fmt.Sprintf("\n\t%s %s %s", "poke-cli", styleBold.Render("types"), "[flag]"),
-			fmt.Sprintf("\n\t%-30s", "Get details about a specific typing"),
-			fmt.Sprintf("\n\t%-30s", "----------"),
-			fmt.Sprintf("\n\t%-30s", styleItalic.Render("Examples:")),
-			fmt.Sprintf("\n\t%-30s", "poke-cli types"),
-			fmt.Sprintf("\n\t%-30s", "A table will then display with the option to select a type."),
-		)
-		fmt.Println(helpMessage)
-	}
-
-	flag.Parse()
-
-	args := os.Args
-
-	err := ValidateTypesArgs(args)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
+// Creating a separate function that handles all the logic for building the table.
+func tableGeneration(endpoint string) table.Model {
 	columns := []table.Column{
 		{Title: "Type", Width: 16},
 	}
@@ -138,15 +104,58 @@ func TypesCommand() {
 	}
 
 	// Type assert to model and access the selected option
-	finalModel := programModel.(model)
+	finalModel, ok := programModel.(model)
+	if !ok {
+		fmt.Println("Error: could not retrieve final model")
+		os.Exit(1)
+	}
+
+	// Check if the user quit the program by pressing 'Q' or 'CTRL-C'
+	if finalModel.selectedOption == "quit" {
+		// Return early to prevent further execution
+		return t
+	}
+
+	// Proceed with the API call only if a type was selected
 	typesName := strings.ToLower(finalModel.selectedOption)
 
-	// Extract the first 4 characters of the endpoint from the argument
-	endpoint := strings.ToLower(args[1])[0:4]
+	baseURL := "https://pokeapi.co/api/v2/"
+	typeResponse, typeName, _ := connections.TypesApiCall(endpoint, typesName, baseURL)
 
-	// Call the selectionTable function with the selected type and endpoint
-	if err := selectionResult(endpoint, typesName); err != nil {
+	capitalizedString := cases.Title(language.English).String(typeName)
+
+	pokemonCount := len(typeResponse.Pokemon)
+
+	fmt.Printf("You selected Type: %s\nNumber of Pokémon with type: %d\n", capitalizedString, pokemonCount)
+	return t
+}
+
+func TypesCommand() {
+	styleBold = lipgloss.NewStyle().Bold(true)
+	styleItalic = lipgloss.NewStyle().Italic(true)
+
+	flag.Usage = func() {
+		helpMessage := helpBorder.Render(
+			styleBold.Render("USAGE:"),
+			fmt.Sprintf("\n\t%s %s %s", "poke-cli", styleBold.Render("types"), "[flag]"),
+			fmt.Sprintf("\n\t%-30s", "Get details about a specific typing"),
+			fmt.Sprintf("\n\t%-30s", "----------"),
+			fmt.Sprintf("\n\t%-30s", styleItalic.Render("Examples:")),
+			fmt.Sprintf("\n\t%-30s", "poke-cli types"),
+			fmt.Sprintf("\n\t%-30s", "A table will then display with the option to select a type."),
+		)
+		fmt.Println(helpMessage)
+	}
+
+	flag.Parse()
+
+	err := ValidateTypesArgs(os.Args)
+	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
+
+	endpoint := strings.ToLower(os.Args[1])[0:4]
+
+	tableGeneration(endpoint)
 }
