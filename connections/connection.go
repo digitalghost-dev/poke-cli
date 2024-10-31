093a863990135ff06d9cb5a45223418ab470feb7
Christian <developer@digitalghost.dev>
2024-10-31 13:58:39 -0700

    updating TypesJSONStruct to include data for damage chart (#62)

--- connections/connection.go
+++ connections/connection.go
@@ -39,6 +39,32 @@ type TypesJSONStruct struct {
 		} `json:"pokemon"`
 		Slot int `json:"slot"`
 	} `json:"pokemon"`
+	DamageRelations struct {
+		DoubleDamageFrom []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"double_damage_from"`
+		DoubleDamageTo []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"double_damage_to"`
+		HalfDamageFrom []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"half_damage_from"`
+		HalfDamageTo []struct {
+			Name string `json:"name"`
+			URL  string `json:"ul"`
+		} `json:"half_damage_to"`
+		NoDamageFrom []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"no_damage_from"`
+		NoDamageTo []struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"no_damage_to"`
+	} `json:"damage_relations"`
 }
 
 var httpGet = http.Get
