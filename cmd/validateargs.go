236de521de9ed9a5af87a92a0c75980fb2b23305
Christian <developer@digitalghost.dev>
2024-11-12 15:34:54 -0800

    reducing redunancy with a helper method (#69)

--- cmd/validateargs.go
+++ cmd/validateargs.go
@@ -6,16 +6,33 @@ import (
 	"os"
 )
 
+func handleHelpFlag(args []string) {
+	if len(args) == 3 && (args[2] == "-h" || args[2] == "--help") {
+		flag.Usage()
+
+		if flag.Lookup("test.v") == nil {
+			os.Exit(0)
+		}
+	}
+}
+
 // ValidatePokemonArgs validates the command line arguments
 func ValidatePokemonArgs(args []string) error {
 
-	if len(args) > 5 {
-		errMessage := errorBorder.Render(errorColor.Render("Error!"), "\nToo many arguments")
+	handleHelpFlag(args)
+
+	if len(args) < 3 {
+		errMessage := errorBorder.Render(
+			errorColor.Render("Error!"),
+			"\nPlease declare a Pokémon's name after the [pokemon] command",
+			"\nRun 'poke-cli pokemon -h' for more details",
+			"\nerror: insufficient arguments",
+		)
 		return fmt.Errorf("%s", errMessage)
 	}
 
-	if len(args) < 3 {
-		errMessage := errorBorder.Render(errorColor.Render("Error!"), "\nPlease declare a Pokémon's name after the [pokemon] command", "\nRun 'poke-cli pokemon -h' for more details", "\nerror: insufficient arguments")
+	if len(args) > 5 {
+		errMessage := errorBorder.Render(errorColor.Render("Error!"), "\nToo many arguments")
 		return fmt.Errorf("%s", errMessage)
 	}
 
@@ -31,16 +48,14 @@ func ValidatePokemonArgs(args []string) error {
 		}
 	}
 
-	if args[2] == "-h" || args[2] == "--help" {
-		flag.Usage()
-		return fmt.Errorf("")
-	}
-
 	return nil
 }
 
 // ValidateTypesArgs validates the command line arguments
 func ValidateTypesArgs(args []string) error {
+
+	handleHelpFlag(args)
+
 	if len(args) > 3 {
 		errMessage := errorBorder.Render(errorColor.Render("Error!"), "\nToo many arguments")
 		return fmt.Errorf("%s", errMessage)
@@ -54,16 +69,7 @@ func ValidateTypesArgs(args []string) error {
 		finalErrorMessage := errorTitle + errorString
 		renderedError := errorBorder.Render(finalErrorMessage)
 		return fmt.Errorf("%s", renderedError)
-
-		// Check if there are exactly 3 arguments and the third argument is either '-h' or '--help'
-		// If true, display the usage information
-	} else if len(args) == 3 && (args[2] == "-h" || args[2] == "--help") {
-		flag.Usage()
-
-		// Only call os.Exit if not in test mode
-		if flag.Lookup("test.v") == nil {
-			os.Exit(0)
-		}
 	}
+
 	return nil
 }
