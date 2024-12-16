2cbff9604c31c66372be1e2f7a4ec96077d73e6c
Christian <developer@digitalghost.dev>
2024-12-16 14:08:39 -0800

    updating tests

--- cli_test.go
+++ cli_test.go
@@ -167,7 +167,7 @@ func TestRunCLI(t *testing.T) {
 		{
 			name:           "Latest Flag",
 			args:           []string{"-l"},
-			expectedOutput: "Latest Docker image version: v0.9.1\nLatest release tag: v0.9.1\n",
+			expectedOutput: "Latest Docker image version: v0.9.2\nLatest release tag: v0.9.2\n",
 			expectedCode:   0,
 		},
 	}
