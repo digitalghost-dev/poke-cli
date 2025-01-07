4f5b9073d9d9fa759142dd9783546a8ee0b5a584
Christian <developer@digitalghost.dev>
2025-01-06 20:20:43 -0800

    adding correct parsing of ImageFlag() (#93)

--- cmd/pokemon.go
+++ cmd/pokemon.go
@@ -15,6 +15,8 @@ import (
 // PokemonCommand processes the Pokémon command
 func PokemonCommand() {
 
+	hintMessage := styleItalic.Render("options: [sm, md, lg]")
+
 	flag.Usage = func() {
 		helpMessage := helpBorder.Render(
 			"Get details about a specific Pokémon.\n\n",
@@ -24,6 +26,8 @@ func PokemonCommand() {
 			"\n\n",
 			styleBold.Render("FLAGS:"),
 			fmt.Sprintf("\n\t%-30s %s", "-a, --abilities", "Prints the Pokémon's abilities."),
+			fmt.Sprintf("\n\t%-30s %s", "-i=xx, --image=xx", "Prints out the Pokémon's default sprite."),
+			fmt.Sprintf("\n\t%5s%-15s", "", hintMessage),
 			fmt.Sprintf("\n\t%-30s %s", "-s, --stats", "Prints the Pokémon's base stats."),
 			fmt.Sprintf("\n\t%-30s %s", "-t, --types", "Prints the Pokémon's typing."),
 			fmt.Sprintf("\n\t%-30s %s", "-h, --help", "Prints the help menu."),
@@ -76,15 +80,22 @@ func PokemonCommand() {
 		capitalizedString, pokemonID, weightKilograms, weightPounds, heightFeet, feet, inches,
 	)
 
-	if *abilitiesFlag || *shortAbilitiesFlag {
-		if err := flags.AbilitiesFlag(endpoint, pokemonName); err != nil {
-			fmt.Printf("Error: %s\n", err)
+	if *imageFlag != "" || *shortImageFlag != "" {
+		// Determine the size based on the provided flags
+		size := *imageFlag
+		if *shortImageFlag != "" {
+			size = *shortImageFlag
+		}
+
+		// Call the ImageFlag function with the specified size
+		if err := flags.ImageFlag(endpoint, pokemonName, size); err != nil {
+			fmt.Println(err)
 			os.Exit(1)
 		}
 	}
 
-	if *imageFlag || *shortImageFlag {
-		if err := flags.ImageFlag(endpoint, pokemonName); err != nil {
+	if *abilitiesFlag || *shortAbilitiesFlag {
+		if err := flags.AbilitiesFlag(endpoint, pokemonName); err != nil {
 			fmt.Printf("Error: %s\n", err)
 			os.Exit(1)
 		}
