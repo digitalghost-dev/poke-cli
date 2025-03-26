a2e9ebd3d5ec6b400fc45a9996f94dfd8a841ec5
Christian <developer@digitalghost.dev>
2025-03-26 09:27:11 -0700

    adding tests

--- cmd/search/model_input_test.go
+++ cmd/search/model_input_test.go
@@ -1,1 +1,30 @@
 package search
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"testing"
+)
+
+func TestUpdateInput(t *testing.T) {
+	ti := textinput.New()
+	ti.SetValue("mewtwo")
+
+	m := Model{
+		ShowResults: true,
+		TextInput:   ti,
+	}
+
+	msg := tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune{'b'}}
+	mUpdated, _ := UpdateInput(msg, m)
+
+	updated := mUpdated.(Model)
+
+	if updated.ShowResults {
+		t.Errorf("expected ShowResults to be false after pressing 'b'")
+	}
+
+	if updated.TextInput.Value() != "" {
+		t.Errorf("expected TextInput to be reset")
+	}
+}
