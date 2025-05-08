ceaf9416ed64af742fdc5035e7ee6143f7fcb001
Christian <developer@digitalghost.dev>
2025-05-08 09:16:13 -0700

    moving files to own packages (#140)

---
+++ cmd/ability/ability.go
@@ -0,0 +1,118 @@
+package ability
+
+import (
+	"flag"
+	"fmt"
+	"github.com/digitalghost-dev/poke-cli/cmd/utils"
+	"github.com/digitalghost-dev/poke-cli/connections"
+	"github.com/digitalghost-dev/poke-cli/flags"
+	"github.com/digitalghost-dev/poke-cli/styling"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"os"
+	"strings"
+)
+
+func AbilityCommand() string {
+	var output strings.Builder
+
+	flag.Usage = func() {
+		helpMessage := styling.HelpBorder.Render(
+			"Get details about a specific ability.\n\n",
+			styling.StyleBold.Render("USAGE:"),
+			fmt.Sprintf("\n\t%s %s %s %s", "poke-cli", styling.StyleBold.Render("ability"), "<ability-name>", "[flag]"),
+			fmt.Sprintf("\n\t%-30s", styling.StyleItalic.Render("Use a hyphen when typing a name with a space.")),
+			"\n\n",
+			styling.StyleBold.Render("FLAGS:"),
+			fmt.Sprintf("\n\t%-30s %s", "-p, --pokemon", "Prints Pokémon that learn this ability."),
+			fmt.Sprintf("\n\t%-30s %s", "-h, --help", "Prints the help menu."),
+		)
+		output.WriteString(helpMessage)
+	}
+
+	abilityFlags, pokemonFlag, shortPokemonFlag := flags.SetupAbilityFlagSet()
+
+	args := os.Args
+
+	flag.Parse()
+
+	if len(os.Args) == 3 && (os.Args[2] == "-h" || os.Args[2] == "--help") {
+		flag.Usage()
+		return output.String()
+	}
+
+	if err := utils.ValidateAbilityArgs(args); err != nil {
+		output.WriteString(err.Error())
+		return output.String()
+	}
+
+	endpoint := strings.ToLower(args[1])
+	abilityName := strings.ToLower(args[2])
+
+	if err := abilityFlags.Parse(args[3:]); err != nil {
+		output.WriteString(fmt.Sprintf("error parsing flags: %v\n", err))
+		abilityFlags.Usage()
+		if os.Getenv("GO_TESTING") != "1" {
+			os.Exit(1)
+		}
+		return output.String()
+	}
+
+	abilitiesStruct, abilityName, err := connections.AbilityApiCall(endpoint, abilityName, connections.APIURL)
+	if err != nil {
+		if os.Getenv("GO_TESTING") != "1" {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		return err.Error()
+	}
+
+	// Extract English short_effect
+	var englishShortEffect string
+	for _, entry := range abilitiesStruct.EffectEntries {
+		if entry.Language.Name == "en" {
+			englishShortEffect = entry.ShortEffect
+			break
+		}
+	}
+
+	// Extract English flavor_text_entries
+	var englishFlavorEntry string
+	for _, entry := range abilitiesStruct.FlavorEntries {
+		if entry.Language.Name == "en" {
+			englishFlavorEntry = entry.FlavorText
+			break
+		}
+	}
+
+	capitalizedAbility := cases.Title(language.English).String(strings.ReplaceAll(abilityName, "-", " "))
+	output.WriteString(styling.StyleBold.Render(capitalizedAbility) + "\n")
+
+	// API is missing some data for the short_effect for abilities from Generation 9.
+	// If short_effect is empty, fallback to the move's flavor_text_entry.
+	if englishShortEffect == "" {
+		output.WriteString("Effect: " + englishFlavorEntry + "\n")
+	} else {
+		output.WriteString("Effect: " + englishShortEffect + "\n")
+	}
+
+	// Print the generation where the move was first introduced.
+	generationParts := strings.Split(abilitiesStruct.Generation.Name, "-")
+	if len(generationParts) > 1 {
+		generationUpper := strings.ToUpper(generationParts[1])
+		output.WriteString("Generation: " + generationUpper + "\n")
+	} else {
+		output.WriteString("Generation: Unknown\n")
+	}
+
+	if *pokemonFlag || *shortPokemonFlag {
+		if err := flags.PokemonAbilitiesFlag(&output, endpoint, abilityName); err != nil {
+			output.WriteString(fmt.Sprintf("error parsing flags: %v\n", err))
+			if os.Getenv("GO_TESTING") != "1" {
+				os.Exit(1)
+			}
+		}
+	}
+
+	return output.String()
+}
---
+++ cmd/natures/natures.go
@@ -0,0 +1,67 @@
+package natures
+
+import (
+	"flag"
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+	"github.com/digitalghost-dev/poke-cli/cmd/utils"
+	"github.com/digitalghost-dev/poke-cli/styling"
+	"os"
+	"strings"
+)
+
+func NaturesCommand() string {
+	var output strings.Builder
+
+	// Define the usage function
+	flag.Usage = func() {
+		helpMessage := styling.HelpBorder.Render(
+			"Get details about all natures.\n\n",
+			styling.StyleBold.Render("USAGE:"),
+			fmt.Sprintf("\n\t%s %s", "poke-cli", styling.StyleBold.Render("natures")),
+		)
+		output.WriteString(helpMessage + "\n")
+	}
+
+	flag.Parse()
+
+	if len(os.Args) == 3 && (os.Args[2] == "-h" || os.Args[2] == "--help") {
+		flag.Usage()
+		return output.String()
+	}
+
+	if err := utils.ValidateNaturesArgs(os.Args); err != nil {
+		output.WriteString(err.Error())
+		return output.String()
+	}
+
+	output.WriteString("Natures affect the growth of a Pokémon.\n" +
+		"Each nature increases one of its stats by 10% and decreases one by 10%.\n" +
+		"Five natures increase and decrease the same stat and therefore have no effect.\n\n" +
+		styling.StyleBold.Render("Nature Chart:") + "\n")
+
+	chart := [][]string{
+		{" ", styling.Red.Render("-Attack"), styling.Red.Render("-Defense"), styling.Red.Render("-Sp. Atk"), styling.Red.Render("-Sp. Def"), styling.Red.Render("Speed")},
+		{styling.Green.Render("+Attack"), "Hardy", "Lonely", "Adamant", "Naughty", "Brave"},
+		{styling.Green.Render("+Defense"), "Bold", "Docile", "Impish", "Lax", "Relaxed"},
+		{styling.Green.Render("+Sp. Atk"), "Modest", "Mild", "Bashful", "Rash", "Quiet"},
+		{styling.Green.Render("+Sp. Def"), "Calm", "Gentle", "Careful", "Quirky", "Sassy"},
+		{styling.Green.Render("Speed"), "Timid", "Hasty", "Jolly", "Naive", "Serious"},
+	}
+
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(styling.Gray)).
+		BorderRow(true).
+		BorderColumn(true).
+		Rows(chart...).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			return lipgloss.NewStyle().
+				Padding(0, 1)
+		})
+
+	output.WriteString(t.Render() + "\n")
+
+	return output.String()
+}
