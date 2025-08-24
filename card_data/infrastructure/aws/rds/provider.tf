a3d3e432782f7d93822cfbe3d2f8b22107945247
Christian Sanchez <developer@digitalghost.dev>
2025-08-23 22:59:01 -0700

    adding hcp terraform cloud provider

--- card_data/infrastructure/aws/rds/provider.tf
+++ card_data/infrastructure/aws/rds/provider.tf
@@ -1,12 +1,21 @@
 provider "aws" {
-	profile = "terraform-user"
-	region = "us-west-2"
+  profile = "terraform-user"
+  region = "us-west-2"
 }
 
 terraform {
-	required_providers {
-		aws = {
-	    version = "~> 6.7.0"
-		}
+  cloud {
+    organization = "digitalghost-dev"
+
+    workspaces {
+      project = "poke-cli"
+      name = "poke-cli"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.7.0"
+    }
   }
 }
