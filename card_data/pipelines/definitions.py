29ec27550922156332bf1153269a6be130000d58
Christian Sanchez <developer@digitalghost.dev>
2025-08-26 09:31:27 -0700

    initial commit

---
+++ card_data/pipelines/definitions.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from dagster import definitions, load_from_defs_folder
+
+
+@definitions
+def defs():
+    return load_from_defs_folder(project_root=Path(__file__).parent.parent)
