f7a7dd173841afc382a705612d7c4e9eed3375f4
Christian Sanchez <developer@digitalghost.dev>
2025-10-04 21:35:08 -0700

    adding table constraints

--- card_data/pipelines/poke_cli_dbt/macros/create_relationships.sql
+++ card_data/pipelines/poke_cli_dbt/macros/create_relationships.sql
@@ -1,5 +1,16 @@
 {% macro create_relationships() %}
-    ALTER TABLE {{ ref('series') }} ADD CONSTRAINT pk_series PRIMARY KEY (id);
-    ALTER TABLE {{ ref('sets') }} ADD CONSTRAINT pk_sets PRIMARY KEY (set_id);
-    ALTER TABLE {{ ref('cards') }} ADD CONSTRAINT pk_cards PRIMARY KEY (id);
+    ALTER TABLE {{ target.schema }}.series ADD CONSTRAINT pk_series PRIMARY KEY (id);
+    ALTER TABLE {{ target.schema }}.sets ADD CONSTRAINT pk_sets PRIMARY KEY (set_id);
+    ALTER TABLE {{ target.schema }}.cards ADD CONSTRAINT pk_cards PRIMARY KEY (id);
+
+    ALTER TABLE public.sets
+    ADD CONSTRAINT fk_sets_series
+    FOREIGN KEY (series_id)
+        REFERENCES public.series (id);
+
+    ALTER TABLE public.cards
+    ADD CONSTRAINT fk_cards_sets
+    FOREIGN KEY (set_id)
+    REFERENCES public.sets (set_id);
+
 {% endmacro %}
