b69eaaa5c245681dd28631359a96a721c0c457ab
Christian Sanchez <developer@digitalghost.dev>
2025-10-08 16:12:09 -0700

    dropping constraints and adding run_query

--- card_data/pipelines/poke_cli_dbt/macros/create_relationships.sql
+++ card_data/pipelines/poke_cli_dbt/macros/create_relationships.sql
@@ -1,16 +1,27 @@
 {% macro create_relationships() %}
-    ALTER TABLE {{ target.schema }}.series ADD CONSTRAINT pk_series PRIMARY KEY (id);
-    ALTER TABLE {{ target.schema }}.sets ADD CONSTRAINT pk_sets PRIMARY KEY (set_id);
-    ALTER TABLE {{ target.schema }}.cards ADD CONSTRAINT pk_cards PRIMARY KEY (id);
+    {{ print("Dropping existing constraints...") }}
 
-    ALTER TABLE public.sets
-    ADD CONSTRAINT fk_sets_series
-    FOREIGN KEY (series_id)
-        REFERENCES public.series (id);
+    -- Drop existing constraints if they exist (in reverse dependency order)
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".cards DROP CONSTRAINT IF EXISTS fk_cards_sets") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".sets DROP CONSTRAINT IF EXISTS fk_sets_series") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".cards DROP CONSTRAINT IF EXISTS pk_cards") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".sets DROP CONSTRAINT IF EXISTS pk_sets") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".series DROP CONSTRAINT IF EXISTS pk_series") %}
 
-    ALTER TABLE public.cards
-    ADD CONSTRAINT fk_cards_sets
-    FOREIGN KEY (set_id)
-    REFERENCES public.sets (set_id);
+    {{ print("Adding primary keys...") }}
 
+    -- Add primary keys
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".series ADD CONSTRAINT pk_series PRIMARY KEY (id)") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".sets ADD CONSTRAINT pk_sets PRIMARY KEY (set_id)") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".cards ADD CONSTRAINT pk_cards PRIMARY KEY (id)") %}
+
+    {{ print("Adding foreign keys...") }}
+
+    -- Add foreign keys
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".sets ADD CONSTRAINT fk_sets_series FOREIGN KEY (series_id) REFERENCES " ~ target.schema ~ ".series (id)") %}
+    {% do run_query("ALTER TABLE " ~ target.schema ~ ".cards ADD CONSTRAINT fk_cards_sets FOREIGN KEY (set_id) REFERENCES " ~ target.schema ~ ".sets (set_id)") %}
+
+    {{ print("Relationships created successfully") }}
+
+    {% do return('') %}
 {% endmacro %}
