7210fc855979352ff44d67e1b22c597c738b1660
Christian Sanchez <developer@digitalghost.dev>
2025-10-19 15:21:39 -0700

    refactor load_pricing_data for explicit asset dependency

--- card_data/pipelines/defs/load/load_pricing_data.py
+++ card_data/pipelines/defs/load/load_pricing_data.py
@@ -1,4 +1,5 @@
 import dagster as dg
+import polars as pl
 from dagster import RetryPolicy, Backoff
 from sqlalchemy.exc import OperationalError
 from ..extract.extract_pricing_data import build_dataframe
@@ -7,17 +8,15 @@ from termcolor import colored
 
 
 @dg.asset(
-    deps=[build_dataframe],
     kinds={"Supabase", "Postgres"},
     retry_policy=RetryPolicy(max_retries=3, delay=2, backoff=Backoff.EXPONENTIAL),
 )
-def load_pricing_data() -> None:
+def load_pricing_data(build_pricing_dataframe: pl.DataFrame) -> None:
     database_url: str = fetch_secret()
     table_name: str = "staging.pricing_data"
 
-    df = build_dataframe()
     try:
-        df.write_database(
+        build_pricing_dataframe.write_database(
             table_name=table_name, connection=database_url, if_table_exists="replace"
         )
         print(colored(" ✓", "green"), f"Data loaded into {table_name}")
