0bce206820b6a1f7cc3cc5567c34e3088c7fbcfe
Christian Sanchez <developer@digitalghost.dev>
2025-10-19 00:01:36 -0700

    adding schedule for pricing_data

--- card_data/pipelines/definitions.py
+++ card_data/pipelines/definitions.py
@@ -5,10 +5,14 @@ from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
 
 import dagster as dg
 
+from .defs.extract.extract_pricing_data import build_dataframe
+from .defs.load.load_pricing_data import load_pricing_data
+
 
 @definitions
 def defs():
-    return load_from_defs_folder(project_root=Path(__file__).parent.parent)
+    folder_defs = load_from_defs_folder(project_root=Path(__file__).parent.parent)
+    return dg.Definitions.merge(folder_defs, defs_pricing)
 
 dbt_project_directory = Path(__file__).absolute().parent / "poke_cli_dbt"
 dbt_project = DbtProject(project_dir=dbt_project_directory)
@@ -23,5 +27,13 @@ dbt_project.prepare_if_dev()
 def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 
-# Dagster object that contains the dbt assets and resource
-defs_dbt = dg.Definitions(assets=[dbt_models], resources={"dbt": dbt_resource})
+price_schedule = dg.ScheduleDefinition(
+    name="price_schedule",
+    cron_schedule="10 10 * * *",
+    target=[build_dataframe, load_pricing_data],
+    execution_timezone="America/Los_Angeles",
+)
+
+defs_pricing = dg.Definitions(
+    schedules=[price_schedule],
+)
