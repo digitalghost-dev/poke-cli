6629cd5d48a4495fc2f5ca01c1fd08537dc284bc
Christian Sanchez <developer@digitalghost.dev>
2025-11-10 14:09:53 -0800

    editing RLS ruling

--- card_data/pipelines/poke_cli_dbt/macros/create_rls.sql
+++ card_data/pipelines/poke_cli_dbt/macros/create_rls.sql
@@ -1,4 +1,9 @@
 {% macro enable_rls() %}
     ALTER TABLE {{ this }} ENABLE ROW LEVEL SECURITY;
-    CREATE POLICY "Enable read access for all users" ON {{ this }} TO PUBLIC USING (true);
+    CREATE POLICY "Enable Read Access for All Users"
+        ON {{ this }}
+        AS PERMISSIVE
+        FOR SELECT
+        TO PUBLIC
+        USING (true);
 {% endmacro %}
