5ab5d54cdb23b47d54781a88d7d8211c375c6847
Christian Sanchez <developer@digitalghost.dev>
2025-12-12 23:36:05 -0800

    normalize `localId` padding and handle missing pricing data

--- card_data/pipelines/poke_cli_dbt/macros/create_view.sql
+++ card_data/pipelines/poke_cli_dbt/macros/create_view.sql
@@ -4,37 +4,37 @@
         WITH cards_cte AS (
             SELECT
                 set_id,
-                image,
                 name,
+                image,
+                illustrator,
                 "localId",
                 "set_cardCount_official",
-                CONCAT(name, ' - ', "localId", '/', LPAD("set_cardCount_official"::text, 3, '0')) AS card_combined_name,
+                CONCAT(name, ' - ', LPAD("localId", 3, '0'), '/', LPAD("set_cardCount_official"::text, 3, '0')) AS card_combined_name,
                 set_name
             FROM public.cards
         ),
-
          cards_pricing_cte AS (
              SELECT
                  product_id,
                  market_price,
-                 CONCAT(name, ' - ', card_number) AS card_combined_name,
+                 CONCAT(REPLACE(name, ' (Secret)', ''), ' - ', card_number) AS card_combined_name,
                  card_number
              FROM public.pricing_data
          )
-
         SELECT
             c.set_id,
             c.name,
-            CONCAT(p.card_number, ' - ', c.name) AS number_plus_name,
+            CONCAT(COALESCE(p.card_number, LPAD(c."localId", 3, '0')), ' - ', c.name) AS number_plus_name,
             CONCAT(c.image, '/high.png') AS image_url,
             c.set_name,
-            c."localId",
+            LPAD(c."localId", 3, '0') AS "localId",
             p."market_price",
-            p."card_number"
+            COALESCE(p."card_number", LPAD(c."localId", 3, '0')) AS card_number,
+            c.illustrator
         FROM
             cards_cte AS c
-                INNER JOIN
+                LEFT JOIN
             cards_pricing_cte AS p
             ON c.card_combined_name = p.card_combined_name
-        ORDER BY c."localId"
+        ORDER BY c."localId"::integer
 {% endmacro %}
