778d7ff79c7e86b00723902f08379936e50ea1d9
Christian Sanchez <developer@digitalghost.dev>
2026-02-13 14:11:15 -0800

    replacing specific validation function with a generic one (#244)

--- cmd/berry/berry.go
+++ cmd/berry/berry.go
@@ -37,7 +37,7 @@ func BerryCommand() (string, error) {
 	flag.Parse()
 
 	// Validate arguments
-	if err := utils.ValidateBerryArgs(os.Args); err != nil {
+	if err := utils.ValidateArgs(os.Args, utils.Validator{MaxArgs: 3, CmdName: "types", RequireName: false, HasFlags: false}); err != nil {
 		output.WriteString(err.Error())
 		return output.String(), err
 	}
