3fac127e550f6da3c147473529b655fffd9ee081
Christian Sanchez <developer@digitalghost.dev>
2026-02-19 21:30:53 -0800

    adding `GenerateHelpMessage()` function (#248)

---
+++ cmd/utils/help.go
@@ -0,0 +1,45 @@
+package utils
+
+import (
+	"fmt"
+
+	"github.com/digitalghost-dev/poke-cli/styling"
+)
+
+type HelpConfig struct {
+	Description    string
+	CmdName        string
+	SubCmdName     string
+	Flags          []FlagHelp
+	ShowHyphenHint bool
+}
+
+type FlagHelp struct {
+	Short, Long string
+	Description string
+}
+
+func GenerateHelpMessage(cfg HelpConfig) string {
+	flagsList := ""
+	for _, f := range cfg.Flags {
+		flagsList += fmt.Sprintf("\n\t%-30s %s", f.Short+", "+f.Long, f.Description)
+	}
+
+	hyphenHint := ""
+	if cfg.ShowHyphenHint {
+		hyphenHint = fmt.Sprintf("\n\t%-30s", styling.StyleItalic.Render(styling.HyphenHint))
+	}
+
+	helpMessage := styling.HelpBorder.Render(
+		fmt.Sprintf("%s\n\n", cfg.Description),
+		styling.StyleBold.Render("USAGE:"),
+		fmt.Sprintf("\n\t%s %s %s", "poke-cli", styling.StyleBold.Render(cfg.CmdName), cfg.SubCmdName),
+		hyphenHint,
+		"\n\n",
+		styling.StyleBold.Render("FLAGS:"),
+		fmt.Sprintf("\n\t%-30s %s", "-h, --help", "Prints the help menu."),
+		flagsList,
+	)
+
+	return helpMessage
+}
