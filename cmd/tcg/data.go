0e54566fd56ff9dd13fa6e292d0053bc4998cc93
Christian Sanchez <developer@digitalghost.dev>
2026-03-19 21:48:33 -0700

    adding 2 columns for standings table (#247)

--- cmd/tcg/data.go
+++ cmd/tcg/data.go
@@ -8,11 +8,13 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type standingRow struct {
+type standingRows struct {
 	Rank          int    `json:"rank"`
 	Name          string `json:"name"`
 	Points        int    `json:"points"`
 	Record        string `json:"record"`
+	OppWinPct     string `json:"opp_win_percent"`
+	OppOppWinPct  string `json:"opp_opp_win_percent"`
 	Deck          string `json:"deck"`
 	PlayerCountry string `json:"player_country"`
 	CountryCode   string `json:"country_code"`
@@ -24,20 +26,20 @@ type standingRow struct {
 }
 
 type standingsDataMsg struct {
-	items []standingRow
+	items []standingRows
 	err   error
 }
 
 func fetchData(tournament string) tea.Cmd {
 	return func() tea.Msg {
-		cols := "rank,name,points,record,deck,player_country,country_code,location,text_date,type,iso_code,player_quantity"
+		cols := "rank,name,points,record,opp_win_percent,opp_opp_win_percent,deck,player_country,country_code,location,text_date,type,iso_code,player_quantity"
 		endpoint := "https://uoddayfnfkebrijlpfbh.supabase.co/rest/v1/standings?select=" + cols + "&location=eq." + url.QueryEscape(tournament) + "&order=rank"
 		body, err := supabaseConn(endpoint)
 		if err != nil {
 			return standingsDataMsg{err: err}
 		}
 
-		var rows []standingRow
+		var rows []standingRows
 		if err = json.Unmarshal(body, &rows); err != nil {
 			return standingsDataMsg{err: err}
 		}
