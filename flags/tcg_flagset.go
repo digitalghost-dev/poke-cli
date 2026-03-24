201875e0c7603b146bb9ee97a8c90758baad244c
Christian Sanchez <developer@digitalghost.dev>
2026-03-23 20:18:43 -0700

    adding `--web` flag to `tcg` command (#249)

---
+++ flags/tcg_flagset.go
@@ -0,0 +1,56 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+	"os/exec"
+	"runtime"
+
+	"github.com/digitalghost-dev/poke-cli/styling"
+)
+
+type TcgFlags struct {
+	FlagSet *flag.FlagSet
+	Web *bool
+	ShortWeb *bool
+}
+
+func SetupTcgFlagSet() *TcgFlags {
+	tf := &TcgFlags{}
+	tf.FlagSet = flag.NewFlagSet("tcgFlags", flag.ExitOnError)
+
+	tf.Web = tf.FlagSet.Bool("web", false, "Opens a Streamlit dashboard of stats in the browser")
+	tf.ShortWeb = tf.FlagSet.Bool("w", false, "Opens a Streamlit dashboard of stats in the browser")
+
+	tf.FlagSet.Usage = func() {
+		helpMessage := styling.HelpBorder.Render(
+			"poke-cli tcg [flags]\n\n",
+			styling.StyleBold.Render("FLAGS:"),
+			fmt.Sprintf("\n\t%-30s %s", "-w, --web", "Opens a Streamlit dashboard of stats in the browser."),
+		)
+		fmt.Println(helpMessage)
+	}
+
+	return tf
+}
+
+func WebFlag(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+		case "windows":
+			cmd = "cmd"
+			args = []string{"/c", "start", url}
+
+		case "darwin":
+			cmd = "open"
+			args = []string{url}
+
+		default:
+			cmd = "xdg-open"
+			args = []string{url}
+	}
+
+	return exec.Command(cmd, args...).Start()
+}
---
+++ flags/tcg_flagset_test.go
@@ -0,0 +1,50 @@
+package flags
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetupTcgFlagSet(t *testing.T) {
+	tf := SetupTcgFlagSet()
+
+	assert.NotNil(t, tf, "Flag set should not be nil")
+	assert.Equal(t, "tcgFlags", tf.FlagSet.Name(), "Flag set name should be 'tcgFlags'")
+
+	flagTests := []struct {
+		flag     interface{}
+		expected interface{}
+		name     string
+	}{
+		{tf.Web, false, "Web flag default should be false"},
+		{tf.ShortWeb, false, "ShortWeb flag default should be false"},
+	}
+
+	for _, tt := range flagTests {
+		assert.NotNil(t, tt.flag, tt.name)
+		assert.Equal(t, tt.expected, reflect.ValueOf(tt.flag).Elem().Interface(), tt.name)
+	}
+}
+
+func TestSetupTcgFlagSet_ParseWebFlag(t *testing.T) {
+	tests := []struct {
+		name string
+		args []string
+		web  bool
+	}{
+		{"long flag", []string{"--web"}, true},
+		{"short flag", []string{"-w"}, true},
+		{"no flag", []string{}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tf := SetupTcgFlagSet()
+			err := tf.FlagSet.Parse(tt.args)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.web, *tf.Web || *tf.ShortWeb)
+		})
+	}
+}
