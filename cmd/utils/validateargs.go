0acef1dc622d19eeeef4074438fd1242abe027cd
Christian Sanchez <developer@digitalghost.dev>
2026-05-07 11:17:31 -0700

    updating argument positions

--- cmd/utils/validateargs.go
+++ cmd/utils/validateargs.go
@@ -20,9 +20,10 @@ func checkLength(args []string, max int) error {
 	return nil
 }
 
-// checkNoOtherOptions checks if there are exactly 3 arguments and the third argument is neither '-h' nor '--help'
+// checkNoOtherOptions checks if a no-flag command received an extra argument
+// and that argument is neither '-h' nor '--help'.
 func checkNoOtherOptions(args []string, max int, commandName string) error {
-	if len(args) == max && args[2] != "-h" && args[2] != "--help" {
+	if len(args) == max && args[max-1] != "-h" && args[max-1] != "--help" {
 		return fmt.Errorf("%s", FormatError(fmt.Sprintf("The only available options after the\n<%s> command are '-h' or '--help'", commandName)))
 	}
 	return nil
@@ -32,7 +33,7 @@ func ValidateArgs(args []string, v Validator) error {
 	if err := checkLength(args, v.MaxArgs); err != nil {
 		return err
 	}
-	if v.RequireName && len(args) == 2 {
+	if v.RequireName && len(args) == 1 {
 		return fmt.Errorf("%s", FormatError(fmt.Sprintf(
 			"Please declare a(n) %s's name after the <%s> command\nRun 'poke-cli %s -h' for more details\nerror: insufficient arguments",
 			v.CmdName, v.CmdName, v.CmdName,
@@ -48,14 +49,13 @@ func ValidateArgs(args []string, v Validator) error {
 
 // ValidatePokemonArgs validates the command line arguments
 func ValidatePokemonArgs(args []string) error {
-	// Check if the number of arguments is less than 3
-	if len(args) < 3 {
+	if len(args) < 2 {
 		return fmt.Errorf("%s", FormatError(
 			"Please declare a Pokémon's name after the <pokemon> command\nRun 'poke-cli pokemon -h' for more details\nerror: insufficient arguments",
 		))
 	}
 
-	if err := checkLength(args, 8); err != nil {
+	if err := checkLength(args, 7); err != nil {
 		return err
 	}
 
@@ -74,9 +74,8 @@ func ValidatePokemonArgs(args []string) error {
 		}
 	}
 
-	// Validate each argument after the Pokémon's name
-	if len(args) > 3 {
-		for _, arg := range args[3:] {
+	if len(args) > 2 {
+		for _, arg := range args[2:] {
 			// Check for an empty flag after Pokémon's name
 			if arg == "-" || arg == "--" {
 				return fmt.Errorf("%s", FormatError(fmt.Sprintf("Empty flag '%s'.\nPlease specify valid flag(s).", arg)))
