ca6d973d5853db1cf7717ce17141292bd305b17c
Christian Sanchez <developer@digitalghost.dev>
2026-05-30 17:43:36 +0200

    adding in `ProfileOptions` struct (#289)

--- services/src/bin/poke-aggregate.rs
+++ services/src/bin/poke-aggregate.rs
@@ -40,15 +40,36 @@ enum ImageSize {
     Lg,
 }
 
+impl From<PokemonArgs> for ProfileOptions {
+    fn from(args: PokemonArgs) -> Self {
+        Self {
+            abilities: args.abilities,
+            defense: args.defense,
+            image: args.image.map(|size| match size {
+                ImageSize::Sm => "sm",
+                ImageSize::Md => "md",
+                ImageSize::Lg => "lg",
+            }.to_string()),
+            moves: args.moves,
+            stats: args.stats,
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let cli: Cli = Cli::parse();
 
     match cli.command{
         SubCommands::Pokemon(args) => {
-            println!("Pokémon name: {}", args.name);
-            println!("Abilities: {}", args.abilities);
+            let name: String = args.name.clone();
+            let options: ProfileOptions = args.into();
+
+            let value: serde_json::Value = run(&name, &options)?;
+
+            serde_json::to_writer_pretty(std::io::stdout(), &value)?;
+            println!();
         }
     }
-    
+
     Ok(())
 }
