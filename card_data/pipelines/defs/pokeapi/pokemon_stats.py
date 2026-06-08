c280271bee5461c979f055f059fd690e5cb2997d
Christian Sanchez <developer@digitalghost.dev>
2026-06-08 11:14:03 -0700

    initial Pokémon data ETL

---
+++ card_data/pipelines/defs/pokeapi/pokemon_stats.py
@@ -0,0 +1,31 @@
+import polars as pl
+from sqlalchemy.exc import OperationalError
+
+from ...utils.secret_retriever import fetch_secret
+
+RESOURCE_NAME: str = "pokemon_stats"
+
+
+def create_dataframe(url: str) -> pl.DataFrame:
+    df = pl.read_csv(url)
+
+    return df
+
+
+def upload_dataframe():
+    database_url: str = fetch_secret()
+    table_name: str = f"staging.vg_{RESOURCE_NAME}"
+    df = create_dataframe(f"https://raw.githubusercontent.com/PokeAPI/pokeapi/master/data/v2/csv/{RESOURCE_NAME}.csv")
+
+    try:
+        df.write_database(
+            table_name=table_name, connection=database_url, if_table_exists="replace"
+        )
+        print(f" ✓ Data loaded into {table_name}")
+    except OperationalError as e:
+        print(f" ✖ Connection error in load_pricing_data():", e)
+        raise
+
+
+if __name__ == "__main__":
+    upload_dataframe()
