a43a7356e6759bda2d01ddfd115d23779c812aac
Christian Sanchez <developer@digitalghost.dev>
2026-06-09 09:25:40 -0700

    updating macro to accept arguments

--- card_data/pipelines/poke_cli_dbt/macros/create_rls.sql
+++ card_data/pipelines/poke_cli_dbt/macros/create_rls.sql
@@ -1,10 +1,10 @@
-{% macro enable_rls() %}
+{% macro enable_rls(role='PUBLIC', policy_name='Enable Read Access for All Users') %}
     ALTER TABLE {{ this }} ENABLE ROW LEVEL SECURITY;
-    DROP POLICY IF EXISTS "Enable Read Access for All Users" ON {{ this }};
-    CREATE POLICY "Enable Read Access for All Users"
+    DROP POLICY IF EXISTS "{{ policy_name }}" ON {{ this }};
+    CREATE POLICY "{{ policy_name }}"
         ON {{ this }}
         AS PERMISSIVE
         FOR SELECT
-        TO PUBLIC
+        TO {{ role }}
         USING (true);
 {% endmacro %}
